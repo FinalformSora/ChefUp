da81aa8d0a47dbc1cde5c8cbd04eb4e606ba275d
Nicolas Sirimongkol <nsirimo@calstatela.edu>
2018-01-17 01:26:50 -0800

    inital commit

---
+++ ChefUp/ViewModels/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChefUp.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [MinLength(4)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        [MaxLength(350, ErrorMessage = "Your message is too long! Shorten it bruh!")]
+        public string Message { get; set; }
+    }
+}
